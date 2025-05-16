359e9a562bbe91f974529f65e18f0a50e6023ac2
Dinakar Shanmugasundaram <dinakarsri111@gmail.com>
2025-05-16 17:10:06 +0530

    Add files via upload

---
+++ JS/Calculator.js
@@ -0,0 +1,8 @@
+let calc = " ";
+function operation(op) {
+  calc += op;
+  return (document.querySelector(".result-p").innervalue = calc);
+}
+function res(check) {
+  document.querySelector(".result-p").innerHTML = check;
+}
